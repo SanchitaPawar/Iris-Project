e0bec6d6a095f848b221bdca4d544916aecd5305
Sanchita Dattatray Pawar <94520836+SanchitaPawar@users.noreply.github.com>
2024-04-09 15:01:43 +0000

    text

--- app.py
+++ app.py
@@ -1,52 +1,47 @@
-# Import all require dependencies
-import numpy as np
+# Import all required dependencies
+import numpy as np 
 import pandas as pd
-import pickle
+import pickle 
 import streamlit as st
 
-
 # Create a user interface using streamlit
-st.set_page_config(page_title='Iris Project Sanchita', layout='wide')
-
+st.set_page_config(page_title='Iris Project Sanchita', 
+                   layout='wide')
 
 # Show the title of app in the body
 st.title('Iris Project - Sanchita Pawar')
 
-# Taking inputs from the user
-sep_len = st.number_input('Sepal Length:', min_value=0.00, step=0.01)
-sep_wid = st.number_input('Sepal Width:', min_value=0.00, step=0.01)
+# Taking input from users
+sep_len = st.number_input('Sepal Length : ', min_value=0.00, step=0.01)
+sep_wid = st.number_input('Sepal Width : ', min_value=0.00, step=0.01)
 pet_len = st.number_input('Petal Length : ', min_value=0.00, step=0.01)
 pet_wid = st.number_input('Petal Width : ', min_value=0.00, step=0.01)
 
-# Adding Submit Button
+# Adding a submit button to the page
 button = st.button('Predict')
 
-
-# After pressing submit bytton
+# After pressing submit button 
 if button:
     # Load preprocessor
     with open('notebook/model.pkl', 'rb') as file1:
-        pre = pickle.load(file1)
+        model = pickle.load(file1)
     # Load the model
     with open('notebook/pre.pkl', 'rb') as file2:
         pre = pickle.load(file2)
-
     # Get the results in dataframe format
     dct = {'sepal_length':[sep_len],
-        'sepal_width':[sep_wid],
-        'petal_length':[pet_len],
-        'petal_width':[pet_wid]
-        }
+           'sepal_width':[sep_wid],
+           'petal_length':[pet_len],
+           'petal_width':[pet_wid]}
     xnew = pd.DataFrame(dct)
-
-    # Preprocess xnew
+    # Preprocess xnew 
     xnew_pre = pre.transform(xnew)
-
-    # Get the predictions along with probabality 
+    # Get the predictions along with probability
     pred = model.predict(xnew_pre)
     prob = model.predict_proba(xnew_pre)
-
+    max_prob = np.max(prob)
     # Show above results in streamlit
-    st.subheader(f'Predicted Species : {pred}')
-    st.subheader(f'Probability:{max_prob:.4f}')
+    st.subheader(f'Predicted Species : {pred[0]}')
+    st.subheader(f'Probability : {max_prob:.4f}')
     st.progress(max_prob)
+    
